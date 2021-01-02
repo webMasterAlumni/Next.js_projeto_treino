cca10e5ba167674007f3ad9d1c7984e40f6a78f5
webMasterAlumni <69198104+webMasterAlumni@users.noreply.github.com>
2021-01-02 17:21:26 -0300

    commit de otimização e atualização

---
+++ pages/sobre.js
@@ -0,0 +1,12 @@
+function Sobre() {
+    return (
+        <div>
+            <div>Sobre</div>
+            <Link href="/">
+                <a >Acessar página Home</a>
+            </Link>
+        </div>
+    )
+}
+
+export default Sobre
